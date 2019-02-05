6e292770fd0895fb7a2722f7d00c3f221112a490
renzodamian <renzojaramillocueva@gmail.com>
2019-02-05 18:38:10 -0500

    Docente Commit

---
+++ src/java/clases/Docente.java
@@ -0,0 +1,15 @@
+package clases;
+
+/**
+ *
+ * @author renzo
+ */
+public class Docente {
+    public int idDocetne;
+    public String nombreDocente;
+    public String apellidoDocente;
+    public String email;
+    public String userDocente;
+    public String passDocente;
+
+}
